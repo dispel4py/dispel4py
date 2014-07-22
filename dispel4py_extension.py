4b7ebb01a11abae996919f72c448ca784821108f
Amy Krause <akrause@Amys-MacBook-Pro.local>
2014-07-22 14:42:20 +0200

    tracking latest updates from VERCE

--- dispel4py_extension.py
+++ dispel4py_extension.py
@@ -12,27 +12,42 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from dispel4py import registry
-reg = registry.initRegistry()
+'''
+The iPython extension for viewing and listing modules from the Dispel4Py registry, 
+and visualising a Dispel4Py graph using Graphviz dot.
+'''
 
-def listPackages(pkg):
+from dispel4py import registry
+from dispel4py import dispel4py_client
+import traceback
+
+def _initRegistry():
+    dispel4py_client.config = dispel4py_client.configure()
+    return dispel4py_client._initRegistry()
+    
+def _listPackages(pkg):
+    reg = _initRegistry()
     try:
         pkgs = reg.listPackages(pkg)
         return [p for p in pkgs if not p.endswith('__impl') and not p.endswith('__gendef') and not p == pkg]
     except:
         objs = []
 
-def listObjects(name):
+def _listObjects(name):
+    reg = _initRegistry()
     try:
         objs = reg.list(name)
     except registry.UnknownPackageException:
         objs = []
     return objs
     
-from dispel4py.workflow_graph import drawDot
+from verce.workflow_graph import drawDot
 from IPython.core.display import display_png
 
 def display(graph):
+    '''
+    Visualises the input graph.
+    '''
     display_png(drawDot(graph), raw=True)    
 
 from IPython.core.magic import (Magics, magics_class, line_magic)
@@ -47,11 +62,11 @@ class Dispel4PyMagics(Magics):
     def dispel4py(self, line):
         command = line.split()
         if command[0] == 'list': 
-            pkgs = listPackages(command[1])
+            pkgs = _listPackages(command[1])
             if pkgs: 
                 print "Packages:"
                 for p in pkgs: print '  ' + p
-            objs = listObjects(command[1])
+            objs = _listObjects(command[1])
             pes = []
             functions = []
             for obj in objs:
@@ -67,12 +82,14 @@ class Dispel4PyMagics(Magics):
                 for f in functions: print '  ' + f
         elif command[0] == 'view':
             try:
+                reg = _initRegistry()
                 source = reg.get_code(command[1])
                 if source is None:
                     print "Resource '%s' not found\n" % name
                 else:
                     print source
             except Exception as err:
+                print traceback.format_exc()
                 print "An error occurred."
         else:
             print "Unknown command '%s'" % line
