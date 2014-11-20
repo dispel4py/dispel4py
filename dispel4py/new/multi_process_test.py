facf94a514b83c307a3e524bdf3162357cb398ca
Amy Krause <a.krause@epcc.ed.ac.uk>
2014-11-20 12:51:23 +0000

    more tests

--- dispel4py/new/multi_process_test.py
+++ dispel4py/new/multi_process_test.py
@@ -1,37 +1,53 @@
-from dispel4py.examples.graph_testing.testing_PEs import TestProducer, TestOneInOneOut
+import argparse
 
-# from dispel4py.new.processor import ShuffleCommunication
-# from multiprocessing import Queue, Process
-#
-# def _processWorker(wrapper):
-#     wrapper.process()
-#
-# conx = Queue()
-#
-# prod = TestProducer()
-# communication = ShuffleCommunication(0, [0], [1])
-# prod_wrapper = MultiProcessingWrapper(0, prod, [ { 'input' : 1 } ] )
-# prod_wrapper.targets = { 'output' : [ ('input', communication)] }
-# prod_wrapper.output_queues = { 1 : conx }
-# prod_proc = Process(target=_processWorker, args=(prod_wrapper,))
-# prod_proc.start()
-#
-# cons = TestOneInOneOut()
-# cons_wrapper = MultiProcessingWrapper(1, cons)
-# cons_wrapper.input_queue = conx
-# cons_proc = Process(target=_processWorker, args=(cons_wrapper,))
-# cons_proc.start()
-#
-# prod_proc.join()
-# cons_proc.join()
-
-from multi_process import process
+from dispel4py.examples.graph_testing.testing_PEs import TestProducer, TestOneInOneOut, TestTwoInOneOut
 from dispel4py.workflow_graph import WorkflowGraph
+from multi_process import process
 
-prod = TestProducer()
-cons1 = TestOneInOneOut()
-cons2 = TestOneInOneOut()
-graph = WorkflowGraph()
-graph.connect(prod, 'output', cons1, 'input')
-graph.connect(cons1, 'output', cons2, 'input')
-process(graph, size=5, inputs={ prod : [ { 'input' : 1 }, { 'input' : 2 }, { 'input' : 3 }  ] } )
+
+args = argparse.Namespace
+args.num = 5
+args.simple = False
+
+def testPipeline():
+    prod = TestProducer()
+    cons1 = TestOneInOneOut()
+    cons2 = TestOneInOneOut()
+    graph = WorkflowGraph()
+    graph.connect(prod, 'output', cons1, 'input')
+    graph.connect(cons1, 'output', cons2, 'input')
+    args = argparse.Namespace
+    args.num = 5
+    args.simple = False
+    process(graph, inputs={ prod : [ {}, {}, {}  ] }, args=args )
+    
+def testSquare():
+    graph = WorkflowGraph()
+    prod = TestProducer(2)
+    cons1 = TestOneInOneOut()
+    cons2 = TestOneInOneOut()
+    last = TestTwoInOneOut()
+    graph.connect(prod, 'output0', cons1, 'input')
+    graph.connect(prod, 'output1', cons2, 'input')
+    graph.connect(cons1, 'output', last, 'input0')
+    graph.connect(cons2, 'output', last, 'input1')
+    args.num = 4
+    process(graph, inputs={ prod : [{}]}, args=args )
+
+def testTee():
+    graph = WorkflowGraph()
+    prod = TestProducer()
+    prev = prod
+    cons1 = TestOneInOneOut()
+    cons2 = TestOneInOneOut()
+    graph.connect(prod, 'output', cons1, 'input')
+    graph.connect(prod, 'output', cons2, 'input')
+    args.num = 3
+    process(graph, inputs={prod: [{}, {}, {}, {}, {}]}, args=args)
+
+print '='*20 + 'PIPELINE' + '='*20 
+testPipeline()
+print '='*20 + 'SQUARE  ' + '='*20 
+testSquare()
+print '='*20 + 'TEE     ' + '='*20 
+testTee()
