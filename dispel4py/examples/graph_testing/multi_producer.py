54157db40de7fb428ec9089b39d0711d368c26d9
Amy Krause <a.krause@epcc.ed.ac.uk>
2015-02-02 15:16:51 +0000

    new test

---
+++ dispel4py/examples/graph_testing/multi_producer.py
@@ -0,0 +1,11 @@
+from dispel4py.examples.graph_testing.testing_PEs import TestMultiProducer, TestOneInOneOut
+from dispel4py.workflow_graph import WorkflowGraph
+
+
+prod = TestMultiProducer()
+cons1 = TestOneInOneOut()
+cons2 = TestOneInOneOut()
+
+graph = WorkflowGraph()
+graph.connect(prod, 'output', cons1, 'input')
+graph.connect(cons1, 'output', cons2, 'input')
