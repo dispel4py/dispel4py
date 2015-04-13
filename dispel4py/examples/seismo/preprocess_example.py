d81a3e6559b618a480dcbb2aabf85c07e71bc998
Amy Krause <a.krause@epcc.ed.ac.uk>
2015-04-13 15:02:59 +0100

    tidying up

--- dispel4py/examples/seismo/preprocess_example.py
+++ dispel4py/examples/seismo/preprocess_example.py
@@ -3,91 +3,116 @@ Reads a number of SAC files and applies a preprocessing chain to the traces.
 
 Run with::
 
-    mpiexec -n 2 python -m dispel4py.worker_mpi dispel4py.examples.seismo.preprocess_example -a graph
+    mpiexec -n 2 dispel4py mpi dispel4py.examples.seismo.preprocess_example\
+        -a graph
 
 or::
 
-    python -m dispel4py.simple_process dispel4py.examples.seismo.preprocess_example -a graph
-    
+    dispel4py simple dispel4py.examples.seismo.preprocess_example -a graph
 '''
 
 from dispel4py.core import GenericPE
-from dispel4py.base import BasePE, IterativePE, ConsumerPE, create_iterative_chain
+from dispel4py.base import IterativePE, ConsumerPE, create_iterative_chain
 from dispel4py.workflow_graph import WorkflowGraph
 
 from whiten import spectralwhitening
-from normalization import onebit_norm, mean_norm, gain_norm
-import numpy as np
+from normalization import mean_norm
 from obspy.core import read as obread
 
+
 # These are the file locations
-ROOT_DIR = '/Users/akrause/VERCE/data/Terracorrelator/'
-IN1=ROOT_DIR + "A25A.TA..BHZ.2011.025.00.00.00.000-2011.026.00.00.39.000.rm.scale-AUTO.SAC"
-IN2=ROOT_DIR + "BMN.LB..BHZ.2011.025.00.00.00.023-2011.026.00.00.38.998.rm.scale-AUTO.SAC"
-R1=ROOT_DIR + "RESP.A25A.TA..BHZ"
-R2=ROOT_DIR + "RESP.BMN.LB..BHZ"
+ROOT_DIR = 'data/'
+IN1 = ROOT_DIR + \
+    "A25A.TA..BHZ.2011.025.00.00.00.000-\
+    2011.026.00.00.39.000.rm.scale-AUTO.SAC"
+IN2 = ROOT_DIR + \
+    "BMN.LB..BHZ.2011.025.00.00.00.023\
+    -2011.026.00.00.38.998.rm.scale-AUTO.SAC"
+R1 = ROOT_DIR + "RESP.A25A.TA..BHZ"
+R2 = ROOT_DIR + "RESP.BMN.LB..BHZ"
 
 # Modify this for larger test runs - each file above is repeated
-NREPEATS=1
+NREPEATS = 1
+
 
 class StreamProducer(GenericPE):
     OUTPUT_NAME = 'output'
+
     def __init__(self):
         GenericPE.__init__(self)
         self._add_output(self.OUTPUT_NAME)
         self.count = 0
+
     def process(self, inputs):
         for i in range(NREPEATS):
             self._read_stream(IN1, R1)
             # self.log('Read stream %s' % IN1)
             self._read_stream(IN2, R2)
             # self.log('Read stream %s' % IN2)
+
     def _read_stream(self, inf, rf):
         st = obread(inf, format='SAC')
         self.write(self.OUTPUT_NAME, [self.count, st, rf])
         self.count += 1
 
+
 class StreamToFile(ConsumerPE):
+
     def __init__(self, file_dest):
         ConsumerPE.__init__(self)
         self.file_dest = file_dest
+
     def _process(self, data):
-        count=data[0]
-        str1=data[1]
+        count = data[0]
+        str1 = data[1]
         try:
             fout = self.file_dest % count
         except TypeError:
-            # maybe there's no "%s" in the string so we ignore count - bad idea?
-            fout = file_dest
+            # maybe there's no "%s" in the string so we ignore count
+            fout = self.file_dest
         str1.write(fout, format='SAC')
         self.log('Wrote file to %s' % fout)
+
 
 def decimate(str1, sps):
     str1.decimate(int(str1[0].stats.sampling_rate/sps))
     return str1
+
+
 def detrend(str1):
     str1.detrend('simple')
     return str1
+
+
 def demean(str1):
     str1.detrend('demean')
     return str1
+
+
 def simulate(str1, paz_remove, pre_filt, seedresp):
     str1.simulate(paz_remove=None, pre_filt=pre_filt, seedresp=seedresp)
     return str1
+
+
 def filter(str1, freqmin=0.01, freqmax=1., corners=4, zerophase=False):
-    str1.filter('bandpass', freqmin=freqmin, freqmax=freqmax, corners=corners, zerophase=zerophase)
+    str1.filter('bandpass', freqmin=freqmin, freqmax=freqmax,
+                corners=corners, zerophase=zerophase)
     return str1
-    
+
+
 class PreTaskPE(IterativePE):
+
     def __init__(self, compute_fn, params={}):
         IterativePE.__init__(self)
         self.compute_fn = compute_fn
         self.params = params
+
     def _process(self, data):
-        count=data[0]
-        str1=data[1]
-        rf=data[2]
-        # this is an odd one - need to put one of the tuple values into the parameters list
+        count = data[0]
+        str1 = data[1]
+        rf = data[2]
+        # this is an odd one
+        # - need to put one of the tuple values into the parameters list
         # not sure how to handle this in general, hard coded for now
         try:
             self.params['seedresp']['filename'] = rf
@@ -98,21 +123,23 @@ class PreTaskPE(IterativePE):
         return [count, result, rf]
 
 
-streamProducer=StreamProducer()
+streamProducer = StreamProducer()
 streamToFile = StreamToFile(ROOT_DIR + 'OUTPUT/NOWHITE/%s_preprocessed.SAC')
-streamToFile.name='StreamToFileNonWhitened'
-streamToFileWhitened = StreamToFile(ROOT_DIR + 'OUTPUT/WHITE/%s_preprocessed.SAC')
-streamToFileWhitened.name='StreamToFileWhitened'
-functions = [ 
-                (decimate, { 'sps' : 4 }), 
-                detrend, 
-                demean, 
-                (simulate, {'paz_remove' : None, 
-                            'pre_filt' : (0.005, 0.006, 30.0, 35.0), 
-                            'seedresp' : {'units': 'VEL'} }),
-                (filter, {'freqmin':0.01, 'freqmax':1., 'corners':4, 'zerophase':False}),
-                (mean_norm, { 'N' : 15 })
-            ]
+streamToFile.name = 'StreamToFileNonWhitened'
+streamToFileWhitened = StreamToFile(
+    ROOT_DIR + 'OUTPUT/WHITE/%s_preprocessed.SAC')
+streamToFileWhitened.name = 'StreamToFileWhitened'
+functions = [
+    (decimate, {'sps': 4}),
+    detrend,
+    demean,
+    (simulate, {'paz_remove': None,
+                'pre_filt': (0.005, 0.006, 30.0, 35.0),
+                'seedresp': {'units': 'VEL'}}),
+    (filter, {'freqmin': 0.01, 'freqmax': 1.,
+              'corners': 4, 'zerophase': False}),
+    (mean_norm, {'N': 15})
+]
 preTask = create_iterative_chain(functions, PreTaskPE)
 whiten = PreTaskPE(spectralwhitening)
 whiten.name = 'PE_whiten'
@@ -121,4 +148,5 @@ graph = WorkflowGraph()
 graph.connect(streamProducer, StreamProducer.OUTPUT_NAME, preTask, 'input')
 graph.connect(preTask, 'output', streamToFile, StreamToFile.INPUT_NAME)
 graph.connect(preTask, 'output', whiten, IterativePE.INPUT_NAME)
-graph.connect(whiten, IterativePE.OUTPUT_NAME, streamToFileWhitened, StreamToFile.INPUT_NAME)
+graph.connect(whiten, IterativePE.OUTPUT_NAME,
+              streamToFileWhitened, StreamToFile.INPUT_NAME)
--- dispel4py/examples/seismo/simple_PEs.py
+++ dispel4py/examples/seismo/simple_PEs.py
@@ -1,5 +1,7 @@
 from dispel4py.seismo.seismo import SeismoPE
-from dispel4py.core import GenericPE, NAME
+from obspy.core import read
+import scipy
+
 
 class StreamProducer(SeismoPE):
     INPUT_NAME = 'file'
@@ -7,29 +9,33 @@ class StreamProducer(SeismoPE):
 
     def __init__(self, numIterations=1):
         SeismoPE.__init__(self)
-        self.inputconnections = { 'file' : { NAME : 'file'}}
+        self._add_input('file')
 
     def getDataStreams(self, inputs):
         '''This method returns a stream with the filename.
         This method is called by the seismo framework to read the input data.
         '''
-        self._timestamp = { 'starttime' : None, 'endtime' : None }
-        self._location = { 'channel' : None, 'network' : None, 'station' : None }
+        self._timestamp = {'starttime': None, 'endtime': None}
+        self._location = {'channel': None, 'network': None, 'station': None}
         inputStr = inputs['file']
-        self.attr =  {'network': None, 'channel': None, 'station': None,
-                 'location': None, 'starttime': None, 'endtime': None,
-                 'sampling_rate': None, 'npts': None}
-        return {"streams": [{'data': inputStr, 'attr' : self.attr}]}
+        self.attr = {'network': None, 'channel': None, 'station': None,
+                     'location': None, 'starttime': None, 'endtime': None,
+                     'sampling_rate': None, 'npts': None}
+        return {"streams": [{'data': inputStr, 'attr': self.attr}]}
 
     def compute(self):
         '''This method reads the file and produces a stream.
         This method is called by the seismo framework to iniate processing.
-        The input data is available as "self.st" or, if the input data is a list, it is stored in "self.streams". 
-        In this example, there is only one piece of input data, the filename, and it is stored in "self.st".
-        Compute is called for each input block, so if there are n filenames this method is called n times.
+        The input data is available as "self.st" or, if the input data is a
+        list, it is stored in "self.streams".
+        In this example, there is only one piece of input data, the filename,
+        and it is stored in "self.st".
+        Compute is called for each input block, so if there are n filenames
+        this method is called n times.
         '''
-        stream=read(self.st)
-        stream.merge() # fills gaps with 0, gap correction should be implemented as PE
+        stream = read(self.st)
+        # fills gaps with 0, gap correction should be implemented as PE
+        stream.merge()
         outputattr = dict(self.attr)
         outputattr['starttime'] = str(stream[0].stats.starttime)
         outputattr['endtime'] = str(stream[0].stats.endtime)
@@ -46,12 +52,14 @@ class StreamProducer(SeismoPE):
         self._location['channel'] = outputattr['channel']
         self._location['station'] = outputattr['station']
 
-        self.outputattr=[outputattr]
+        self.outputattr = [outputattr]
         self.outputstreams = [stream[0].data]
 
+
 class DetrendPE(SeismoPE):
-     def __init__(self):
-         SeismoPE.__init__(self)
-     def compute(self):
-         return scipy.signal.detrend(self.st, type='linear')   
-    
+
+    def __init__(self):
+        SeismoPE.__init__(self)
+
+    def compute(self):
+        return scipy.signal.detrend(self.st, type='linear')
