5d209e95a00e23726a399822c2f56c1bf73c502f
Amy Krause <a.krause@epcc.ed.ac.uk>
2015-04-13 11:57:25 +0100

    flake8

--- dispel4py/examples/graph_testing/__init__.py
+++ dispel4py/examples/graph_testing/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) The University of Edinburgh 2014
+# Copyright (c) The University of Edinburgh 2014-2015
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
