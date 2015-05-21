551ce1ceb00a49bea9c46a4bf12a70fc285b9ee4
Amy Krause <a.krause@epcc.ed.ac.uk>
2015-05-21 16:43:16 +0100

    restructuring entry point

---
+++ dispel4py/__main__.py
@@ -0,0 +1,31 @@
+import argparse
+from importlib import import_module
+import traceback
+
+
+def main(args=None):
+    
+#    from dispel4py.new.mappings import config
+    
+    parser = argparse.ArgumentParser(
+        description='Submit a dispel4py graph for processing.')
+    parser.add_argument('target', help='target execution platform')
+    args, remaining = parser.parse_known_args()
+    try:
+        from dispel4py.new import mappings
+        # see if platform is in the mappings file as a simple name
+        target = mappings.config[args.target]
+    except KeyError:
+        # it is a proper module name - fingers crossed...
+        target = args.target
+    try:
+        process = getattr(import_module(target), 'main')
+        process()
+    except:
+        print traceback.format_exc()
+        print 'Unknown target: %s' % target
+        
+
+
+if __name__ == "__main__":
+    main()
