1a872f9154de1e622ef19aff5035dad31496f9e3
Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2016-08-04 19:23:16 +0100

    Delete launch-undet.sh

--- dispel4py/examples/provenance/launch-undet.sh
+++
@@ -1,5 +0,0 @@
-cd ../../../
-pwd
-#python -m dispel4py.new.processor simple undetermined-tst 
-python -m dispel4py.new.processor multi dispel4py.examples.provenance.undetermined_tst -n 20 -f dispel4py/examples/provenance/undetermined-input
-#mpiexec-openmpi-mp python -m dispel4py.new.processor mpi undetermined-tst -n 12 -f undetermined-input 
